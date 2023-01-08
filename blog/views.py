9be60c3b2f243931a4926f3ab1f4deb924dd8b04
Monique Parnis <monique.parnis@gmail.com>
2023-01-08 19:11:10 +0000

    Create, Query & Register Post model

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Aperlae',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 7, 2023'
-    },
-    {
-        'author': 'Maria',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 8, 2023'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
